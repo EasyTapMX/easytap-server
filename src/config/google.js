bf733ca1b1591d17ee177c4511b916eee254a6cf
EasyTapMX <easytap.mx@gmail.com>
2026-07-24 17:31:43 -0600

    Use environment credentials

--- src/config/google.js
+++ src/config/google.js
@@ -1,22 +1,21 @@
 const { google } = require("googleapis");
-const path = require("path");
+
+const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS);
 
 const auth = new google.auth.GoogleAuth({
-    keyFile: path.join(__dirname, "../../credentials.json"),
+    credentials,
     scopes: [
         "https://www.googleapis.com/auth/spreadsheets"
     ]
 });
 
 async function getSheets() {
-
     const client = await auth.getClient();
 
     return google.sheets({
         version: "v4",
         auth: client
     });
-
 }
 
 module.exports = {
